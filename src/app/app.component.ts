00096a853a5847f7dc643adc95c414e936d838d2
Pós <h050706@stud.u-szeged.hu>
2022-04-29 22:15:16 +0200

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'Forum';
-  page = 'gallery';
-
+export class AppComponent implements OnInit {
+  //title = 'Forum';
+  page = '';
+  
+  routes: Array<string> = [];
+  
+  
+  ngOnInit(){
+    this.routes = this.router.config.map(conf => conf.path) as string[];
+    
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((evts: any) =>{
+      const currentPage = (evts.urlAfterRedirects as string).split('/')[1] as string;
+      if(this.routes.includes(currentPage)){
+        this.page = currentPage;
+      }
+    });
+  }
+  
+  constructor(private router: Router){}
+  
   changePage(selectedPage: string){
-    this.page = selectedPage;
+    this.router.navigateByUrl(selectedPage);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './pages/main/main.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { ContactComponent } from './pages/contact/contact.component';
+//import { MainComponent } from './pages/main/main.component';
+//import { GalleryComponent } from './pages/gallery/gallery.component';
+//import { ContactComponent } from './pages/contact/contact.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FormsModule } from '@angular/forms';
-import { ViewerComponent } from './pages/gallery/viewer/viewer.component';
-import { ListComponent } from './pages/gallery/list/list.component';
-import { DateFormatPipe } from './shared/pipes/date-format.pipe';
+//import { ViewerComponent } from './pages/gallery/viewer/viewer.component';
+//import { ListComponent } from './pages/gallery/list/list.component';
+//import { DateFormatPipe } from './shared/pipes/date-format.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    GalleryComponent,
-    ContactComponent,
+    //MainComponent,
+    //GalleryComponent,
+    //ContactComponent,
     MenuComponent,
-    ViewerComponent,
-    ListComponent,
-    DateFormatPipe
+    //ViewerComponent,
+    //ListComponent,
+    //DateFormatPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
